8baa8329abbce4d6960752007c0b2df78e77c548
warddalston <ward.dalston@gmail.com>
2014-03-19 12:49:42 -0500

    Put some more information into the summary.
    A better summary not tells us which model has the highest model odds.

--- A6summaryBMA.R
+++ A6summaryBMA.R
@@ -6,12 +6,12 @@
 #' 
  #' @return A list containing the following elements
 #' \itemize{
-#'  \item{MetaData}{ A length 3 numeric vector containing the number of observations in the BMA analysis, the number of regressions ran, and the value of g, the hyper prior.}
+#'  \item{MetaData}{ A length 4 numeric vector containing the number of observations in the BMA analysis, the number of regressions ran, and the value of g, the hyper prior, and the variables included in the most likely model, along with the model odds for that model.}
 #'  \item{ExpectedBetas}{ A numeric vector with the expected values for the coefficients}
 #' \item{BetaNonZeroProbs}{ A numeric vector with the probability of being non-zero for each coefficient.} 
 #'  }
 #' 
-#' @note This function prints to the console some meta deta about a BMA analysis, and then the expected value and probability of being non zero for each coefficient.  It also silently returns this same information as a list.
+#' @note This function prints to the console some meta deta about a BMA analysis, and then the expected value and probability of being non zero for each coefficient.  It also silently returns this same information as a list.  If the most likely mode includes output is simply "Vars:" this means that the null model is most likely.  
 #' @author Dalston G. Ward \email{ward.dalston@@gmail.com}
 #' @seealso \code{\link{fitBMA}}
 #' @seealso \code{\link{getBMAinput}}
@@ -28,11 +28,15 @@ setMethod(f="summary",
             cat("N observations:",length(object@y),"\n")
             cat("N regressions:", ncol(object@coefficients),"\n")
             cat("g hyper-prior:", object@g,"\n")
+            cat("Most likely model includes", names(object@PostMO)[which.max(object@PostMO)],"Pr =",max(object@PostMO), "\n")
             cat("****************************** \n")
             cat(paste(format(c("Variable",rownames(object@coefficients)),width=10,justify="left"),format(c("E(B)",round(object@PostEB,3)),width=8,justify="right"), format(c("Pr(B!=0)",round(object@PostBetaNonZero,3)),width=10,justify="right"),"\n"),sep="")
             cat("****************************** \n")
-            MetaData <- c(length(object@y),ncol(object@coefficients),object@g)
-            names(MetaData) <- c("N Observations","N Regressions","g hyper-prior")
+            MetaData <- c(length(object@y),ncol(object@coefficients),object@g,max(object@PostMO))
+            names(MetaData) <- c("N Observations","N Regressions","g hyper-prior",names(object@PostMO)[which.max(object@PostMO)])
             invisible(list(MetaData=MetaData,ExpectedBetas=object@PostEB,BetaNonZeroProbs=object@PostBetaNonZero))
           }
 )
+SHHH <- summary(yay)
+
+
